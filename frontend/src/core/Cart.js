8e75d2c38fcf3faa10fc668dd9c3aba8d0b62674
Carlos <carlosarturo.agelvisvergara@connect.qut.edu.au>
2020-01-26 12:07:09 +1000

    show products in cart page

--- frontend/src/core/Cart.js
+++ frontend/src/core/Cart.js
@@ -1,17 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import Layout from './Layout';
-import { getCart } from './cartHelpers';
-import Card from './Card';
-import Checkout from './Checkout';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Layout from "./Layout";
+import { getCart } from "./cartHelpers";
+import Card from "./Card";
 
 const Cart = () => {
     const [items, setItems] = useState([]);
-    const [run, setRun] = useState(false);
 
     useEffect(() => {
         setItems(getCart());
-    }, [run]);
+    }, []);
 
     const showItems = items => {
         return (
@@ -19,15 +17,7 @@ const Cart = () => {
                 <h2>Your cart has {`${items.length}`} items</h2>
                 <hr />
                 {items.map((product, i) => (
-                    <Card
-                        key={i}
-                        product={product}
-                        showAddToCartButton={false}
-                        cartUpdate={true}
-                        showRemoveProductButton={true}
-                        setRun={setRun}
-                        run={run}
-                    />
+                    <Card key={i} product={product} />
                 ))}
             </div>
         );
@@ -46,12 +36,15 @@ const Cart = () => {
             className="container-fluid"
         >
             <div className="row">
-                <div className="col-6">{items.length > 0 ? showItems(items) : noItemsMessage()}</div>
+                <div className="col-6">
+                    {items.length > 0 ? showItems(items) : noItemsMessage()}
+                </div>
 
                 <div className="col-6">
-                    <h2 className="mb-4">Your cart summary</h2>
-                    <hr />
-                    <Checkout products={items} setRun={setRun} run={run} />
+                    <p>
+                        show checkout options/shipping address/total/update
+                        quantity
+                    </p>
                 </div>
             </div>
         </Layout>
